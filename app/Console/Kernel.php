85831e0745e9259ef10331307b718b506922e06b
sald19 <carl.sald19@gmail.com>
2017-01-13 18:11:56 -0600

    stop run command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('verify-site-migration-has-changed')
-                  ->everyFiveMinutes();
+//         $schedule->command('verify-site-migration-has-changed')
+//                  ->everyFiveMinutes();
     }
 
     /**
